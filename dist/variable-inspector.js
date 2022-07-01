4eb7882d84e14317a3bb4dc4e5c09acd07b40ee6
2cheol <godicheol@gmail.com>
2022-07-01 17:15:43 +0900

    1

--- dist/variable-inspector.js
+++ dist/variable-inspector.js
@@ -99,9 +99,7 @@
             return new RegExp("("+reg+")").test(arg);
         } else if (typeof(reg) === "object" && reg !== null) {
             if (!Array.prototype.find) {
-                var keys = Object.keys(reg);
-                var vals = keys.map(function(el) {return reg[el];});
-                return vals.indexOf(arg) > -1;
+                return Object.keys(reg).map(function(el){return reg[el]}).indexOf(arg) > -1;
             } else {
                 return Object.keys(reg).find(function(el) {return reg[el] === arg;});
             }
