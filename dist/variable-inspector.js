b31d315b559161412c20a0b67528b4feeb538472
2cheol <godicheol@gmail.com>
2022-07-01 17:13:55 +0900

    1

--- dist/variable-inspector.js
+++ dist/variable-inspector.js
@@ -98,7 +98,10 @@
         } else if (typeof(reg) === "string" || typeof(reg) === "number") {
             return new RegExp("("+reg+")").test(arg);
         } else if (typeof(reg) === "object" && reg !== null) {
-            return Object.keys(reg).find(function(el) {return reg[el] === arg;});
+            var keys = Object.keys(reg);
+            var vals = keys.map(function(el) {return reg[el];});
+            return vals.indexOf(arg) > -1;
+            // return Object.keys(reg).find(function(el) {return reg[el] === arg;});
         } else {
             return false;
         }
@@ -106,45 +109,5 @@
 
     if (typeof(window.variableInspector) === "undefined") {
         window.variableInspector = exports;
-    }
-
-    // polyfills
-    if (!Array.prototype.find) {
-        Object.defineProperty(Array.prototype, 'find', {
-          value: function(predicate) {
-            // 1. Let O be ? ToObject(this value).
-            if (this == null) {
-                throw new TypeError('"this" is null or not defined');
-            }
-            var o = Object(this);
-            // 2. Let len be ? ToLength(? Get(O, "length")).
-            var len = o.length >>> 0;
-            // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-            if (typeof predicate !== 'function') {
-                throw new TypeError('predicate must be a function');
-            }
-            // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-            var thisArg = arguments[1];
-            // 5. Let k be 0.
-            var k = 0;
-            // 6. Repeat, while k < len
-            while (k < len) {
-                // a. Let Pk be ! ToString(k).
-                // b. Let kValue be ? Get(O, Pk).
-                // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-                // d. If testResult is true, return kValue.
-                var kValue = o[k];
-                if (predicate.call(thisArg, kValue, k, o)) {
-                return kValue;
-                }
-                // e. Increase k by 1.
-                k++;
-            }
-            // 7. Return undefined.
-            return undefined;
-            },
-            configurable: true,
-            writable: true
-        });
     }
 })();
